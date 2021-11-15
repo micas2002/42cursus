5745ea4235767e959a528cdf504afe09e899ab56
Miguel Bernardinho goncalves de sousa <mibernar@c1r14s5.42lisboa.com>
2021-11-15 14:18:15 +0000

    more corrections

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,48 +6,42 @@
 /*   By: mibernar <mibernar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/26 13:57:23 by mibernar          #+#    #+#             */
-/*   Updated: 2021/11/14 17:35:59 by mibernar         ###   ########.fr       */
+/*   Updated: 2021/11/15 11:05:19 by mibernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char	*join(char *str, char *dest, char *src)
+static char	join(char *str, const char *s1, const char *s2)
 {
-	int		x;
-	int		y;
+	int	x;
+	int	y;
 
-	y = 0;
 	x = 0;
-	while (str)
+	y = 0;
+	while (s1[x] != '\0')
 	{
-		while (dest)
-		{
-			str[x] = dest[x];
-			x++;
-		}
-		while (src)
-		{
-			str[x] = src[y];
-			x++;
-			y++;
-		}
+		str[x] = s1[x];
+		x++;
 	}
-	return (str);
+	while (s2[y] != '\0')
+	{
+		str[x] = s2[y];
+		x++;
+		y++;
+	}
+	str[x] = '\0';
+	return (*str);
 }
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*str;
-	char	*dest;
-	char	*src;
 
-	dest = s1;
-	src = s2;
 	str = (char *)malloc(sizeof(char)
-			* (strlen((char *)(s1)) + strlen((char *)(s2)) + 1));
+			* (ft_strlen((char *)(s1)) + ft_strlen((char *)(s2)) + 1));
 	if (!str)
 		return (NULL);
-	join(str, dest, src);
+	join(str, s1, s2);
 	return (str);
 }
--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,51 +6,32 @@
 /*   By: mibernar <mibernar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/21 10:57:28 by mibernar          #+#    #+#             */
-/*   Updated: 2021/11/14 16:45:39 by mibernar         ###   ########.fr       */
+/*   Updated: 2021/11/15 13:26:56 by mibernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_strcmp(const char *str, const char *to_find, int x)
-{
-	int	y;
-
-	y = 0;
-	while (to_find[y] != '\0')
-	{
-		if (to_find[y] == str[x])
-		{
-			x++;
-			y++;
-		}
-		else
-			return (0);
-	}
-	return (1);
-}
-
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	x;
+	size_t	i;
 
+	x = 0;
+	if (needle[0] == '\0')
+		return ((char *)haystack);
 	while (haystack[x] != '\0' && x < len)
 	{
 		if (haystack[x] == needle[0])
 		{
-			if (ft_strcmp(haystack, needle, x) == 1)
-			{
-				return ((char *)haystack + x);
-			}
-			else
-				return (NULL);
-		}
-		else if (needle[0] == '\0')
-		{
-			return ((char *)haystack);
+			i = 1;
+			while (needle[i] != '\0' && haystack[x + i] == needle[i] &&
+				(size_t)(x + i) < len)
+				++i;
+			if (needle[i] == '\0')
+				return ((char *) &haystack[x]);
 		}
 		x++;
-		len--;
 	}
-	return (0);
+	return (NULL);
 }
