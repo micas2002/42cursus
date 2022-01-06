1e79dbc99b72e184578d7db87cd54666495c3328
micas2002 <miguelbernardinosousa2002@gmail.com>
2022-01-06 16:50:43 +0000

    started printf

---
+++ printf/ft_printf.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mibernar <mibernar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/06 16:04:18 by mibernar          #+#    #+#             */
+/*   Updated: 2022/01/06 16:26:42 by mibernar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stddef.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <stdarg.h>
+
+// PROTOTYPES
+
+int	ft_printf(const char *, ...);
+
+#endif
